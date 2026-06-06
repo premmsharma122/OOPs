6b63a507541f79712c75aa837ed5f4f506654c46
premmsharma122 <sonusharma4237@gmail.com>
2026-06-06 18:42:57 +0530

    Day 8 of LLD Practice and learning - Payment Gateway design

---
+++ Day8/day8.java
@@ -0,0 +1,60 @@
+package Day8;
+interface PaymentStrategy{
+    void pay(double amount);
+}
+class CreditCardPayment implements PaymentStrategy{
+    CreditCardPayment(){};
+
+    @Override
+    public void pay(double amount){
+        System.out.println("Payment of "+amount+" done by credit-card.");
+    }
+
+}
+
+class UPIPayment implements PaymentStrategy{
+    UPIPayment(){};
+
+    @Override
+    public void pay(double amount){
+        System.out.println("Payment of "+amount+" done by UPI.");
+    }
+    
+}
+class NetBankingPayment implements PaymentStrategy{
+    NetBankingPayment(){};
+
+    @Override
+    public void pay(double amount){
+        System.out.println("Payment of "+amount+" done by NETBanking.");
+    }
+}
+class ShoppingCart{
+    private PaymentStrategy paymentStrategy;
+    void setPaymentStrategy(PaymentStrategy paymentStrategy){
+        this.paymentStrategy=paymentStrategy;
+    }
+
+    void checkout(double amount){
+        if(paymentStrategy==null){
+            System.out.println("Please  select a Payment method first.");
+            return;
+        }
+        paymentStrategy.pay(amount);
+    }
+}
+public class day8 {
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        cart.setPaymentStrategy(new UPIPayment());
+        cart.checkout(1000);
+        System.out.println();
+
+        cart.setPaymentStrategy(new CreditCardPayment());
+        cart.checkout(1000);
+        System.out.println();
+
+        cart.setPaymentStrategy(new NetBankingPayment());
+        cart.checkout(1000);
+    }
+}
